ce70d094fdad5b02a7245ae04a8ef6eaf2d641d4
Ken Horlador <kenhorlador@gmail.com>
2022-02-23 23:22:38 +0800

    add documentation to insert method

--- Data Structures/[5] Trees/Binary Search Tree(2).js
+++ Data Structures/[5] Trees/Binary Search Tree(2).js
@@ -15,6 +15,7 @@ class BinarySearchTree {
   insert(value) {
     const newNode = new Node(value)
 
+    // if there's no root node, set the newNode to root node
     if (!this.root) {
       this.root = newNode
 
@@ -23,23 +24,29 @@ class BinarySearchTree {
 
     const current = this.root
 
+    // iterate through the tree DS
     while(true) {
+      // return undefined when the value to be inserted, is equal to the current node's value
       if (value === current.value) return undefined
 
+      // if the value to be inserted is greater than the current node's value,
       if (value > current.value) {
+        // check whether the current node's right is null, if it is, set the right to the newNode
         if (current.right === null) {
           current.right = newNode
 
           return this
         }
+        // if left is not null, set the current node to current node's right
         current = current.right
-      } else {
+      } else { // if the value of the current node's value is less than the current node's value,
+        // check whether the left is null, if it is, set the left to the newNode then return the tree DS
         if (current.left === null) {
           current.left = newNode
 
           return this
         }
-
+        // if left is not null, set the current to current's left
         current = current.left
       }
     }
