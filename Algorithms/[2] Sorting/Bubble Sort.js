60f19af1b89992fbcba188e2f4279a53d77b781b
Ken Horlador <kenhorlador@gmail.com>
2022-03-01 17:24:12 +0800

    optimized bubble sort for almost sorted arrays

--- Algorithms/[2] Sorting/Bubble Sort.js
+++ Algorithms/[2] Sorting/Bubble Sort.js
@@ -4,26 +4,35 @@ function swap(array, j, i) {
 
 function bubbleSort1(array) {
   const swap = (array, id1, id2) => [array[id1], array[id2] = array[id2], array[id1]]
+  let noSwaps
 
   for (let i = array.length; i > 0; i--) {
+    noSwaps = true
     for (let j = 0; j < i - 1; j++) {
-      if (array[i] > array[i + 1]) swap(array, i, j)
-
+      if (array[i] > array[i + 1]) {
+        swap(array, i, j)
+        noSwaps = false
+      }
     }
+    if (noSwaps) break
   }
 
   return array
 }
 
 function bubbleSort2(array) {
+  let noSwaps
   for (let i = array.length; i > 0; i--) {
+    noSwaps = true
     for (let j = 0; j < i - 1; j++) {
       if (array[j] > array[j + 1]) {
         let temp = array[j]
         array[j] = array[j+1]
         array[j+1] = temp
+        noSwaps = false
       }
     }
+    if (noSwaps) break
   }
 
   return array
