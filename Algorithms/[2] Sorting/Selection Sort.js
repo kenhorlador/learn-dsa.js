25e74ed58ebaf5ed7c092ad1098bc9f6f7367569
Ken Horlador <kenhorlador@gmail.com>
2022-03-02 23:55:18 +0800

    add docs in selection sort

--- Algorithms/[2] Sorting/Selection Sort.js
+++ Algorithms/[2] Sorting/Selection Sort.js
@@ -1,12 +1,16 @@
+// Selection sort is an algorithm that iterates through the array and finds the lowest value and insert it in
+// the first element of the array through swapping.
+
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
     let lowest = i
 
     for (let j = i + 1; j < array.length; j++) {
       if (array[j] < array[lowest]) {
-        lowest = j
+        lowest = j // set the lowest to j.. if the index j < is less than index in lowest
       }
     }
+    // prevents swapping if the value of i and lowest is the same
     if (i !== lowest) {
       let temp = array[i]
       array[i] = array[lowest]
