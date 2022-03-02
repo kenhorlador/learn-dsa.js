2c261177e5300997c62b14bd8c43fed0a48d476a
Ken Horlador <kenhorlador@gmail.com>
2022-03-02 23:51:19 +0800

    add docs in bubble sort's noSwap var

--- Algorithms/[2] Sorting/Bubble Sort.js
+++ Algorithms/[2] Sorting/Bubble Sort.js
@@ -2,19 +2,23 @@ function swap(array, j, i) {
   return [array[i], array[j] = array[j], array[i]]
 }
 
+// Bubble sort is an algorithm that bubbles or swaps the highest value at the very end of the array, then sements it there
+// The only difference between the two implementations of bubble sort is the swap
 function bubbleSort1(array) {
   const swap = (array, id1, id2) => [array[id1], array[id2] = array[id2], array[id1]]
   let noSwaps
 
   for (let i = array.length; i > 0; i--) {
-    noSwaps = true
+    noSwaps = true // every iteration, the noSwaps will be true / this is great on the on the nearly sorted arrays
+    // it will not have to iterate through the array if the array is already sorted
+
     for (let j = 0; j < i - 1; j++) {
       if (array[i] > array[i + 1]) {
         swap(array, i, j)
-        noSwaps = false
+        noSwaps = false // and there's a swap, set it to false
       }
     }
-    if (noSwaps) break
+    if (noSwaps) break // if there's no swap, break out of the loop
   }
 
   return array
