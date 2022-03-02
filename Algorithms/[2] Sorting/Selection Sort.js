c0a977ce028eb0cdb326e1d93e36f7760b1d10fe
Ken Horlador <kenhorlador@gmail.com>
2022-03-02 22:59:44 +0800

    implemented a selection sort with es6 syntax

--- Algorithms/[2] Sorting/Selection Sort.js
+++ Algorithms/[2] Sorting/Selection Sort.js
@@ -25,3 +25,28 @@ selectionSort(array)
 // 25, 1 = 4
 // 25, 42 = 4
 // 25, 55 = 4
+
+function selectionSort1(array) {
+  const swap = (arr, i, j) => (
+    [arr[i], arr[j] = arr[j], arr[i]]
+  )
+
+  for (let i = 0; i < array.length; i++) {
+    let lowest = i
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[lowest]) {
+        lowest = j
+      }
+    }
+    if (i !== lowest) swap(array, i, lowest)
+  }
+
+  return array
+}
+
+// 0, 1 = 1
+// 0, 2 = 1
+// ...
+// [25, 21, 73, 49, 1, 42, 55]
+selectionSort1(array)
