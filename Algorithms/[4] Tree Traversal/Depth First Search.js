f7396cf002db48fae3bb1a1bc744669b9753c931
Ken Horlador <kenhorlador@gmail.com>
2022-03-12 22:23:39 +0800

    implement depth first search in order

--- Algorithms/[4] Tree Traversal/Depth First Search.js
+++ Algorithms/[4] Tree Traversal/Depth First Search.js
@@ -121,6 +121,19 @@ class BinarySearchTree {
 
     return data
   }
+
+  dfSearchInOrder() {
+    let data = []
+
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      data.push(node.value)
+      if (node.right) traverse(node.right)
+    }
+    traverse(this.root)
+
+    return data
+  }
 }
 
 const bstree = new BinarySearchTree()
