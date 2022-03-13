4a8b9f34f2605e7167d0b3a1140f691522105fac
Ken Horlador <kenhorlador@gmail.com>
2022-03-13 23:06:07 +0800

    fix two similar variable name declaration

--- Data Structures/[0] Arrays/Array Methods.js
+++ Data Structures/[0] Arrays/Array Methods.js
@@ -11,13 +11,13 @@ array.pop() // extracts an item from the end of the array
 
 // Splice - Removing items from the array => returns removed items from array
 array.splice([include-start], [exclude-end]) // removes item/s from index 'start' to 'end'
-const array = [1,2,3,4]
-const newArray = array.splice(0,2) // returns [1,2]
+const array1 = [1,2,3,4]
+const newArray = array1.splice(0,2) // returns [1,2]
 
 // Slice - Removing items from the array => returns the original array not including removed items
 array.slice([including-start], [excluding-end])
-const array = [1,2,3,4]
-array.slice(0,2) // returns [3,4]
+const array2 = [1,2,3,4]
+array2.slice(0,2) // returns [3,4]
 
 // We can  also use slice to duplicate an existing array
 const duplicateArray = array.slice() // dont include a parameter, then it creates a duplicate of the original array
