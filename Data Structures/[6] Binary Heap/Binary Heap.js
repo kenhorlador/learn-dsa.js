57bb9c7586ce19881ae4879065872ddd9bddc1af
Ken Horlador <kenhorlador@gmail.com>
2022-03-16 22:46:59 +0800

    add docs to binary heap

--- Data Structures/[6] Binary Heap/Binary Heap.js
+++ Data Structures/[6] Binary Heap/Binary Heap.js
@@ -1,3 +1,11 @@
+// Binary Heap is a data structure that stores data in a tree like structure
+// The only rule it has is that, all parent node's value is greater than its child/children
+// It fills the left first before the right
+// To find the left child, we use 2n + 1
+// To find the right child, we use 2n + 2
+// To find the parent, we use n - 1 / 2 then floor it
+// With that idea, we can be able to store heaps in array
+
 class MaxBinaryHeap {
   constructor() {
     this.data = [41,39,33,18,27,12]
@@ -11,19 +19,23 @@ class MaxBinaryHeap {
     console.log(this.data)
   }
 
+  // A function that pushes the value of in the heap
   _bubbleUp() {
     let index = this.data.length - 1
-    const element = this.data[index]
+    const element = this.data[index] // last Element
 
     while (index > 0) {
       let parentIndex = Math.floor((index - 1) / 2)
-      let parent = this.data[parentIndex]
+      let parent = this.data[parentIndex] // parent element
 
+      // break out of the loop if current element is less than or equal to parent
       if (element <= parent) break
 
-      this.data[parentIndex] = element
-      this.data[index] = parent
-      index = parentIndex
+      // Will only run if parent is greater than element
+      // Swap parent element with current element
+      this.data[parentIndex] = element // set parent to current
+      this.data[index] = parent // set current to parent
+      index = parentIndex // update index, to avoid infinitely checking the same value
     }
   }
 }
