d7ed87a938506621e97c949a452577b0d89008f1
Ken Horlador <kenhorlador@gmail.com>
2022-03-21 22:24:48 +0800

    implement quick sort

--- Algorithms/[2] Sorting/Quick Sort.js
+++ Algorithms/[2] Sorting/Quick Sort.js
@@ -19,3 +19,19 @@ function pivot(arr, start = 0, end = arr.length - 1) {
 }
 
 let arr = [4,8,2,1,7,5,6,3]
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+
+  if (left < right) {
+
+    let pivotIdx = pivot(arr, left, right)
+
+    // left
+    quickSort(arr, left, pivotIdx - 1)
+
+    // right
+    quickSort(arr, pivotIdx + 1, right)
+  }
+
+  return arr
+}
