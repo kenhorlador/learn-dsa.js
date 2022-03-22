e3ffbe860a99a122d24df41393dd829ec6d3875e
Ken Horlador <kenhorlador@gmail.com>
2022-03-22 22:02:57 +0800

    add docs

--- Algorithms/[2] Sorting/Quick Sort.js
+++ Algorithms/[2] Sorting/Quick Sort.js
@@ -1,4 +1,10 @@
+
+// pivot chooses an element to be evaluated
+// filters all the high values in right and low values in left
+
 function pivot(arr, start = 0, end = arr.length - 1) {
+
+  // swap function
   const swap = (array, i, j) => {
     return [array[i], array[j] = array[j], array[i]]
   }
@@ -6,13 +12,16 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   let pivot = arr[start]
   let swapIdx = start
 
-  for (let i = start + 1; arr.length; i++) {
+  // loop starts at 'start + 1' to avoid evaluating itself
+  for (let i = start + 1; i < arr.length; i++) {
+    // if pivot is greater than the current element, so we have to swap it to left
     if (pivot > arr[i]) {
       swapIdx++
       swap(arr, swapIdx, i)
     }
   }
 
+  // after the entire loop, swap the pivot's index to the last element + 1 that was swapped
   swap(arr, swapIdx, start)
 
   return swapIdx
