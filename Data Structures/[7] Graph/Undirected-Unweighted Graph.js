9d0f15199dae7345e73b660f32169b3bbbab4caa
Ken Horlador <kenhorlador@gmail.com>
2022-03-24 16:33:46 +0800

    implement removeEdge method

--- Data Structures/[7] Graph/Undirected-Unweighted Graph.js
+++ Data Structures/[7] Graph/Undirected-Unweighted Graph.js
@@ -2,18 +2,26 @@
 
 class Graph {
   constructor() {
-    this.adjencencyList = {}
+    this.adjacencyList = {}
   }
 
   addVertex(vertex) {
-    if (!this.adjencencyList[vertex])this.adjencencyList[vertex] = []
+    if (!this.adjacencyList[vertex])this.adjacencyList[vertex] = []
   }
 
 
   addEdge(v1, v2) {
-    this.adjencencyList[v1].push(v2)
-    this.adjencencyList[v2].push(v1)
+    this.adjacencyList[v1].push(v2)
+    this.adjacencyList[v2].push(v1)
   }
 
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => {
+      v !== vertex2
+    })
 
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => {
+      v !== vertex1
+    })
+  }
 }
