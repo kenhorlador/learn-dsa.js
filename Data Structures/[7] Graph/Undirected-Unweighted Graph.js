c6f77a54110f4c03e96c272dd31a55ef41233aa3
Ken Horlador <kenhorlador@gmail.com>
2022-03-26 21:00:46 +0800

    add function identifier

--- Data Structures/[7] Graph/Undirected-Unweighted Graph.js
+++ Data Structures/[7] Graph/Undirected-Unweighted Graph.js
@@ -5,16 +5,18 @@ class Graph {
     this.adjacencyList = {}
   }
 
+  // add nodes function
   addVertex(vertex) {
     if (!this.adjacencyList[vertex])this.adjacencyList[vertex] = []
   }
 
-
+  // add connection
   addEdge(v1, v2) {
     this.adjacencyList[v1].push(v2)
     this.adjacencyList[v2].push(v1)
   }
 
+  // remove connection
   removeEdge(vertex1, vertex2) {
     this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => {
       v !== vertex2
@@ -25,6 +27,7 @@ class Graph {
     })
   }
 
+  // remove node function
   removeVertex(vertex) {
     while (this.adjacencyList[vertex].length) {
       const adjacentVertex = this.adjacencyList[vertex].pop()
