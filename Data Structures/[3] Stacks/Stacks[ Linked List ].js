336bdc3ae433251301bd78f96c921d6a5212bcfe
Ken Horlador <kenhorlador@gmail.com>
2022-03-27 23:41:22 +0800

    implement stack using doubly linkedlist

--- Data Structures/[3] Stacks/Stacks[ Linked List ].js
+++ Data Structures/[3] Stacks/Stacks[ Linked List ].js
@@ -12,3 +12,61 @@
   push - O(1) - Add an item at the end
   peek - O(1) - View the topmost item
 */
+
+// Stack back
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+    this.previous = null
+  }
+}
+
+class Stacks {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  // push a node at the back
+  push(value) {
+    const newNode = new Node(vallue)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.previous = this.tail
+      this.tail = newNode
+    }
+
+    return ++this.length
+  }
+
+  // remove a node at the back
+  pop() {
+
+    if (!this.head) return null
+
+    const temp = this.tail
+
+    if (this.length === 1) {
+
+      temp = this.head
+      this.tail = null
+      this.head = null
+
+    } else {
+
+      this.tail = this.tail.previous
+
+    }
+
+    this.length--
+
+    return temp
+  }
+}
